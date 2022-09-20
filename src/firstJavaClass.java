91c9f7086f2f1fdd80f4da01313de21b84fea633
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-20 10:50:18 -0500

    initial commit for stuff and things

---
+++ src/firstJavaClass.java
@@ -0,0 +1,17 @@
+public class firstJavaClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
+
+// single line comment
+/* multi line comment
+
+ */
+
+/**
+ * documentation comment or w/e tf he said
+ */
+
